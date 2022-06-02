4948a40811527f9285d90d11e3fdc4ae93671d2b
Cristian Pirnog <cristian.pirnog@quantco.com>
2022-06-02 17:06:26 +0200

    Made InterfaceMQTTShelly subclass of InterfaceMQTT This alolws control via the homeassistant MQTT topics. Moved the pip packages to the proper place in environment.yaml

--- picframe/interface_mqtt_shelly.py
+++ picframe/interface_mqtt_shelly.py
@@ -7,9 +7,9 @@ import json
 import os
 from picframe import __version__
 from picframe.controller import Controller
+from interface_mqtt import InterfaceMQTT as InterfaceMQTTBase
 
-
-class InterfaceMQTT:
+class InterfaceMQTT(InterfaceMQTTBase):
     """MQTT interface of picframe.
 
     This interface interacts via mqtt with the user to steer the image display via Shelly
@@ -27,44 +27,9 @@ class InterfaceMQTT:
     topic_prefix = "shellies"
 
 
-    def __init__(self, controller, mqtt_config):
-        self.__logger = logging.getLogger(self.__class__.__name__)
-        self.__logger.info('creating an instance of InterfaceMQTT')
-        self.__controller = controller
-        try:
-            device_id = mqtt_config['device_id']
-            self.__client = mqtt.Client(client_id = device_id, clean_session=True)
-            login = mqtt_config['login']
-            password = mqtt_config['password']
-            self.__client.username_pw_set(login, password)
-            tls = mqtt_config['tls']
-            if tls:
-                self.__client.tls_set(tls)
-            server = mqtt_config['server']
-            port = mqtt_config['port']
-            self.__client.on_connect = self.on_connect
-            self.__client.on_message = self.on_message
-            self.__client.connect(server, port, 60)
-            # self.__device_id = mqtt_config['device_id']
-        except Exception as e:
-            self.__logger.info("MQTT not set up because of: {}".format(e))
-
-    def start(self):
-        try:
-            self.__controller.publish_state = self.publish_state
-            self.__client.loop_start()
-        except Exception as e:
-            self.__logger.info("MQTT not started because of: {}".format(e))
-
-    def stop(self):
-        try:
-            self.__controller.publish_state = Controller.noop_publish_state
-            self.__client.loop_stop()
-        except Exception as e:
-            self.__logger.info("MQTT stopping failed because of: {}".format(e))
-
-
     def on_connect(self, client, userdata, flags, rc):
+        super().on_connect(client, userdata, flags, rc)
+        
         if rc != 0:
             self.__logger.warning("Can't connect with mqtt broker. Reason = {0}".format(rc))
             return
@@ -83,7 +48,7 @@ class InterfaceMQTT:
     def on_message(self, client, userdata, message):
         self.__logger.info('on_message called')
         if not message.topic.startswith(self.topic_prefix):
-            self.__logger.info(f'Ignoring message "{message.payload}" from topic "{message.topic}"')
+            super().on_message(client, userdata, message)
             return
 
         self.__logger.info(f'Message topic: {message.topic}')
@@ -123,8 +88,8 @@ class InterfaceMQTT:
             self.__logger.info('Showing the text')
             self.__controller.set_show_text()
         elif msg['event'] == 'L':
-            self.__logger.info('Turning the display on')
-            self.__controller.display_is_on = True
+            self.__logger.info('Toggling the display')
+            self.__controller.display_is_on = not self.__controller.display_is_on
 
     def publish_state(self, image, image_attr):
         pass
